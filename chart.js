a16d0b87a17907ee3e55755e0f5c65a48561edbd
david <davidlai9@gmail.com>
2021-07-29 17:07:29 -0700

    reduce graph size

--- chart.js
+++ chart.js
@@ -159,8 +159,10 @@ class HistoricalPriceChart {
     this.bollingerBandsData = this.calculateBollingerBands(validData, 19);
 
     this.margin = { top: 50, right: 50, bottom: 50, left: 20 };
-    this.width = window.innerWidth - this.margin.left - this.margin.right; // Use the window's width
-    this.height = window.innerHeight - this.margin.top - this.margin.bottom; // Use the window's height
+    // this.width = window.innerWidth - this.margin.left - this.margin.right; // Use the window's width
+    this.width = 1200;
+    // this.height = window.innerHeight - this.margin.top - this.margin.bottom; // Use the window's height
+    this.height = 700;
 
     // find data range
     const xMin = d3.min(this.currentData, d => d['date']);
